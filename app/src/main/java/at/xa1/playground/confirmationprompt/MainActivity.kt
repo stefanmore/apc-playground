79e8957179fab7bda13a17d55feb7b7cd0860a2f
Stefan <stefan.more@iaik.tugraz.at>
2024-10-22 16:59:13 +0200

    forked, added signing

--- app/src/main/java/at/xa1/playground/confirmationprompt/MainActivity.kt
+++ app/src/main/java/at/xa1/playground/confirmationprompt/MainActivity.kt
@@ -2,8 +2,16 @@ package at.xa1.playground.confirmationprompt
 
 import android.os.Bundle
 import android.security.ConfirmationPrompt
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyGenParameterSpec.Builder
+import android.security.keystore.KeyProperties
 import androidx.appcompat.app.AppCompatActivity
 import at.xa1.playground.confirmationprompt.databinding.ActivityMainBinding
+import java.security.KeyPairGenerator
+import java.security.KeyStore
+import java.security.spec.ECGenParameterSpec
+import java.util.Date
+import javax.security.auth.x500.X500Principal
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,6 +38,8 @@ class MainActivity : AppCompatActivity() {
         }
 
         showWelcomeMessage()
+        genKeypair()
+        logView.log("Now its your turn ...\n")
     }
 
     private fun showWelcomeMessage() {
@@ -39,7 +49,7 @@ class MainActivity : AppCompatActivity() {
                 "${ConfirmationPrompt.isSupported(this@MainActivity)}\n" +
                 "press button show ConfirmationPrompt once, " +
                 "long press to call showConfirmationPrompt twice.\n"+
-                "---"
+                "---\n"
         )
     }
 
@@ -54,11 +64,50 @@ class MainActivity : AppCompatActivity() {
 
     override fun onPause() {
         super.onPause()
-        logView.log("Activity.onPause")
+        logView.log("Activity.onPause\n")
     }
 
     override fun onStop() {
         super.onStop()
-        logView.log("Activity.onStop")
+        logView.log("Activity.onStop\n")
+    }
+
+    fun genKeypair() {
+        var keyAliasName = "TestKey"
+        val builder = KeyGenParameterSpec.Builder(keyAliasName, KeyProperties.PURPOSE_SIGN)
+            .setUserConfirmationRequired(true)
+            //.setUserAuthenticationRequired(true)
+            .setUnlockedDeviceRequired(true)
+            .setCertificateSubject(X500Principal(String.format("CN=%s, OU=KUL, OU=DISTIRNET, C=BE", keyAliasName)))
+            .setCertificateNotBefore(Date())
+            .setKeyValidityStart(Date())
+            .setDigests(KeyProperties.DIGEST_SHA256,
+                KeyProperties.DIGEST_SHA384,
+                KeyProperties.DIGEST_SHA512)
+            .setAlgorithmParameterSpec(ECGenParameterSpec("secp256r1"));
+
+        // generate key in Android keystore
+        val keyPairGenerator = KeyPairGenerator.getInstance(KeyProperties.KEY_ALGORITHM_EC, "AndroidKeyStore")
+
+        keyPairGenerator.initialize(builder.build());
+        val keyPair = keyPairGenerator.generateKeyPair()
+        logView.log("New key \"$keyAliasName\" generated!\n" +
+                    "--- \n")
+
+        getKeys()
+
+    }
+
+    fun getKeys() {
+        val keyStore = KeyStore.getInstance("AndroidKeyStore")
+        keyStore.load(null)
+        val aliases = keyStore.aliases()
+
+        logView.log("Keys in AndroidKeyStore: ")
+        for (alias in aliases) {
+            val entry = keyStore.getEntry(alias, null)
+            logView.log("- $alias \n" +
+                        "---")
+        }
     }
 }
--- app/src/main/java/at/xa1/playground/confirmationprompt/ShowConfirmationPrompt.kt
+++ app/src/main/java/at/xa1/playground/confirmationprompt/ShowConfirmationPrompt.kt
@@ -5,7 +5,14 @@ import android.security.ConfirmationAlreadyPresentingException
 import android.security.ConfirmationCallback
 import android.security.ConfirmationNotAvailableException
 import android.security.ConfirmationPrompt
+import com.fasterxml.jackson.dataformat.cbor.databind.CBORMapper
+import java.security.KeyPair
+import java.security.KeyStore
+import java.security.PrivateKey
+import java.security.Signature
+import java.util.Map
 import java.util.concurrent.Executor
+
 
 fun showConfirmationPrompt(
     context: Context,
@@ -22,14 +29,83 @@ fun showConfirmationPrompt(
 
     val extraData: ByteArray = extraDataString.toByteArray()
     val threadReceivingCallback = Executor { runnable -> runnable.run() }
+    val cborMapper = CBORMapper()
+
     val callback = object : ConfirmationCallback() {
         override fun onConfirmed(dataThatWasConfirmed: ByteArray) {
             super.onConfirmed(dataThatWasConfirmed)
             logView.log(
                 "${logPrefix}onConfirmed: dataThatWasConfirmed =\n" +
                     "      hex: ${dataThatWasConfirmed.toHex()}\n" +
-                    "      escaped ascii: ${dataThatWasConfirmed.toAsciiOrHex()}"
+                    "      escaped ascii: ${dataThatWasConfirmed.toAsciiOrHex()}  \n" +
+                    "   \n"
             )
+
+            val decodedData = cborMapper.readValue(dataThatWasConfirmed, Map::class.java)
+
+            logView.log("${logPrefix}onConfirmed: CBOR decoded data ")
+            for (entry in decodedData.entrySet()) {
+                val value =
+                    when (entry.value) {
+                        is String -> { entry.value as String }
+                        is ByteArray -> { (entry.value as ByteArray).toAsciiOrHex() }
+                        else -> { entry.value.toString() }
+                    }
+                logView.log("- ${entry.key}: ${value}")
+            }
+
+
+            // sign data that was confirmed (only possible once):
+            signData(dataThatWasConfirmed)
+
+            // now try to sign something that was not confirmed:
+            //dataThatWasConfirmed.set(10, 'D'.toByte())
+            //signData(dataThatWasConfirmed)
+
+            //dataThatWasConfirmed.set(10, 'M'.toByte())
+            //signData(dataThatWasConfirmed)
+        }
+
+        private fun signData(dataThatWasConfirmed: ByteArray) {
+            logView.log("${logPrefix}signing with KeyStore ...")
+            logView.log("escaped ascii: ${dataThatWasConfirmed.toAsciiOrHex()} ")
+
+            try {
+                val signature = initSignature("TestKey");
+                checkNotNull(signature)
+                signature.update(dataThatWasConfirmed)
+                val signatureBytes = signature.sign()
+                checkNotNull(signatureBytes)
+            }catch (e: Exception) {
+                logView.error("${logPrefix}signing with KeyStore throws", e)
+            }
+
+            logView.log("${logPrefix}signing done!")
+        }
+
+        private fun initSignature(keyName: String): Signature? {
+            var keyPair = getKeyPair(keyName)
+            if (keyPair != null) {
+                val signature: Signature = Signature.getInstance("SHA256withECDSA")
+                signature.initSign(keyPair.getPrivate())
+                return signature
+            }
+            return null
+        }
+
+        private fun getKeyPair(keyName: String): KeyPair? {
+            val keyStore = KeyStore.getInstance("AndroidKeyStore")
+            keyStore.load(null)
+
+            if (keyStore.containsAlias(keyName)) {
+                // Get public key
+                val publicKey = keyStore.getCertificate(keyName).publicKey
+                // Get private key
+                val privateKey = keyStore.getKey(keyName, null) as PrivateKey
+                // Return a key pair
+                return KeyPair(publicKey, privateKey)
+            }
+            return null
         }
 
         override fun onDismissed() {
